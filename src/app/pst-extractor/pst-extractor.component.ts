10b478ca935f5f78414f9320cc667888082ce788
arunrajkkv <arunrajkkv@gmail.com>
2024-03-30 10:45:52 +0530

    #101 - Enhancements - Added the fetures of file choose and extra header data

--- src/app/pst-extractor/pst-extractor.component.ts
+++ src/app/pst-extractor/pst-extractor.component.ts
@@ -18,13 +18,14 @@ export class PstExtractorComponent implements OnInit {
     modalData: any = {};
     headerData: any = {};
     fileName: string = '';
+    imagesArray: any = [];
 
     constructor(private _commonService: CommonService) { }
 
     ngOnInit() {
     }
 
-    getData() {
+    public getData(): void {
         let directoryPath = 'D:\\others\\pst files\\';
         const FILE = directoryPath + this.fileName;
         this._commonService.getExtractedData(FILE)
@@ -33,13 +34,7 @@ export class PstExtractorComponent implements OnInit {
                     if (response) {
                         this.extractedData = response;
                         this.isShowFolderView = true;
-                        if (this.extractedData && this.extractedData.length > 0) {
-                            this.extractedData.forEach((data: any) => {
-                                if (data.messages.length > 0) {
-                                    this.messageArray = data.messages;
-                                }
-                            });
-                        }
+                        this.setContentForTableView();
                     }
                 },
                 error => {
@@ -48,16 +43,28 @@ export class PstExtractorComponent implements OnInit {
                 });
     }
 
-    onClickFolder(data: any): void {
+    private setContentForTableView(): void {
+        let tempMessageArray: any = [];
+        if (this.extractedData && this.extractedData.length > 0) {
+            this.extractedData.forEach((data: any) => {
+                if (data.messages.length > 0) {
+                    tempMessageArray.push(data.messages);
+                }
+            });
+            this.messageArray = tempMessageArray[0];
+        }
+    }
+
+    public onClickFolder(data: any): void {
         this.isShowContentData = true;
         this.headerData = data.header_data;
     }
 
-    onSelectPstFile(event: any) {
+    public onSelectPstFile(event: any): void {
         this.fileName = event ? event.srcElement.files[0].name : '';
     }
 
-    convertToDateObject(dateString: string): any {
+    public convertToDateObject(dateString: string): any {
         if (dateString) {
             const uint8Array = new TextEncoder().encode(dateString);/* Convert the string to a Uint8Array */
             const timestamp = new DataView(uint8Array.buffer).getUint32(0, true);/* Extract the timestamp from the Uint8Array */
@@ -66,6 +73,13 @@ export class PstExtractorComponent implements OnInit {
             date.setTime(milliseconds);
             return date;
         }
+    }
+
+    onClickTable(msg: any) {
+      this.imagesArray = [];
+      if (msg && msg.images.length > 0) {
+        this.imagesArray = msg.images;
+      }
     }
 
 }
